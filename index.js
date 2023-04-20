2244b3a07aab1c3006f600c1b3c62c7e5568efe5
rscavuslu <samet.cavuslu@hotmail.com>
2023-04-20 16:52:30 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+console.log(num1 * num2)
+
+const random = Math.floor(Math.random() * 10) + 1;
+console.log(random)
+
+let num3 = 10
+let num4 = 6
+const mod = num3 % num4;
+console.log(mod)
+
+const max = Math.max(20, 2, 3, 4, 5);
+console.log(max)
